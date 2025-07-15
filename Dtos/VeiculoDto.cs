e62f74f4f6670c3d60a7f5b9d64c2ede185e5815
caian <caianhenrique11@gmail.com>
2025-07-15 19:41:18 -0400

    veiculo validation

--- Dtos/VeiculoDto.cs
+++ Dtos/VeiculoDto.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
+using api_teste.Validations;
+
 namespace api_teste.Dtos
 {
     public class VeiculoDto
@@ -57,54 +59,5 @@ namespace api_teste.Dtos
         [Required]
         [Range(1, 10000, ErrorMessage = "Valor da diária deve estar entre 1 e 10.000.")]
         public float ValorDiaria { get; set; } = 0!;
-    }
-
-    public class PlacaBrAttribute : RegularExpressionAttribute
-    {
-        private const string PlacaRegex = @"^([A-Z]{3}-\d{4}|[A-Z]{3}\d[A-Z]\d{2})$";
-        public PlacaBrAttribute()
-            : base(PlacaRegex)
-        {
-            ErrorMessage = "Placa inválida. Use ABC-1234 ou o padrão Mercosul (ABC1D23).";
-        }
-    }
-
-    public class RenavamBrAttribute : ValidationAttribute
-    {
-        public RenavamBrAttribute()
-        {
-            ErrorMessage = "RENAVAM inválido.";
-        }
-
-        public override bool IsValid(object? value)
-        {
-            if (value is not string s) return false;
-            s = Regex.Replace(s, @"\D", "").PadLeft(11, '0');
-            if (s.Length != 11) return false;
-
-            var dígitos = s.Select(c => c - '0').ToArray();
-            int[] pesos = { 2, 3, 4, 5, 6, 7, 8, 9, 2, 3 };
-            int soma = 0;
-            for (int i = 0; i < 10; i++)
-                soma += dígitos[i] * pesos[i];
-
-            int resto = soma % 11;
-            int dv = (resto < 2) ? 0 : 11 - resto;
-            return dv == dígitos[10];
-        }
-    }
-
-    public class ChassiBrAttribute : ValidationAttribute
-    {
-        private static readonly Regex _rx = new Regex("^[A-HJ-NPR-Z0-9]{17}$", RegexOptions.Compiled);
-        public ChassiBrAttribute()
-        {
-            ErrorMessage = "Chassi inválido. Deve ter 17 caracteres alfanuméricos (sem I, O, Q).";
-        }
-
-        public override bool IsValid(object? value)
-        {
-            return value is string s && _rx.IsMatch(s.ToUpper());
-        }
     }
 }
