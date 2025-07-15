2032b27d74256ba927bc955af533b8bd17ffea58
Thalys Braga <thalys.braga.2018@gmail.com>
2025-07-15 17:51:41 -0400

    Imp: models de cidade, estado, endereço e pessoa

---
+++ Models/Cidade.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace api_teste.Models
+{
+    [Table("cidade")]
+    public class Cidade
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; } = null!;
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonIgnore]
+        [Column("uf_id")]
+        public int UfId { get; set; }
+        [ForeignKey("UfId")]
+        public Estado? Uf { get; set; }
+    }
+}
