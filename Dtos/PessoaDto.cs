407465681ec11e4ae2450ef8f77b39366e58b137
Thalys Braga <thalys.braga.2018@gmail.com>
2025-07-15 16:39:24 -0400

    imp: CRUD pessoa

---
+++ Dtos/PessoaDto.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_teste.Dtos
+
+public class PessoaDto
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Nome { get; set; } = null!;
+
+    [Required]
+    [StringLength(150)]
+    [EmailAddress(ErrorMessage = "E‑mail em formato inválido.")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [RegularExpression(@"^\d{11}$", ErrorMessage = "CPF deve conter exatamente 11 dígitos numéricos.")]
+    public string Cpf { get; set; } = null!;
+
+    [Required]
+    public string Senha { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime DataNascimento { get; set; }
+
+    [Required]
+    public bool Status { get; set; }
+
+    [Required]
+    public int EnderecoId { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime CreatedAt { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_teste.Models
+{
+    [Table("pessoa")]
+    public class Pessoa
+    {
+        public int id { get; set; }
+        public string nome { get; set; } = null!;
+        public string email { get; set; } = null!;
+        public string cpf { get; set; } = null!;
+        public string senha { get; set; } = null!;
+
+        public DateTime data_nascimento { get; set; }
+
+        public bool status { get; set; }
+
+        public int endereco { get; set; }
+
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+
+         [ForeignKey("endereco")]
+        public Endereco? EnderecoNavigation { get; set; }
+    }
+}
